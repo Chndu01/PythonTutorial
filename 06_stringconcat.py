0a6540e464081c75c0842cb1b25e360b2dae2f2b
Chndu01 <contactchandrakantt@gmail.com>
2025-02-28 22:02:41 +0530

    New File

---
+++ 06_stringconcat.py
@@ -0,0 +1,6 @@
+first_Name = "Chandrakant"
+last_Name = "Netam"
+full_Name = first_Name +" "+ last_Name  # String concatenation
+print(full_Name)
+print("Chandrakant " + str( 3))  # Using str funtion to cancatenate another type of data with string
+
